6dff0f575ac2813535d1cba7fee2409723f83126
GAME1.1 <GAME1.1@etpa.local>
2020-11-13 12:48:26 +0100

    Update TriAlgo.py

--- TriAlgo.py
+++ TriAlgo.py
@@ -1,1 +1,30 @@
+def selectMin(list):
+    var=list|[0]
+    for i in list:
+        if var>i
+            var=i 
+    return var
 
+def triSelection(list):
+    result=[]
+    while len(list)>0
+        var=selectMin(list)
+        list.remove(var)
+        result.append(var)
+    return result
+
+    print(selectMin([4,2,7,8]))
+
+
+
+
+def triFusion(list):
+    if len(list)>1:
+    
+        list0= list[0:1]
+        list1= list[2:3]
+    l1=triFusion(list0)
+    l2=triFusion(list1)
+        return fusion(l1,l2)
+    else:
+        return list
